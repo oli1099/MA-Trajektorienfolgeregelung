6836614d4006f6c36cf2bbd6734d144b8877a134
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-02-01 18:06:08 +0100

    Trajektoriefolgeregelung

---
+++ workspace/ros2_ws/build/trajektorienfolgeregelung/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/oli/Desktop/Oliver/Uni/MA/RepoTrajektorienfolgeregelung/MA-Trajektorienfolgeregelung/workspace/ros2_ws/install/trajektorienfolgeregelung'
