5653e3025e0688ef79235d157108bbb8ff4a40ea
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-02-18 16:00:04 +0100

    Launch hinzugefügt

--- workspace/ros2_ws/src/MPC_Trajektorienfolgeregelung/setup.py
+++ workspace/ros2_ws/src/MPC_Trajektorienfolgeregelung/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'MPC_Trajektorienfolgeregelung = MPC_Trajektorienfolgeregelung.mpc_trajectory:main'
         ],
     },
 )
