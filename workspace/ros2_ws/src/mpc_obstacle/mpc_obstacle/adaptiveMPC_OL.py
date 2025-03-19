e7dafa9f844bb85c427c121986fe804fd8705835
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-03-19 10:11:35 +0100

    EDIT

--- workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_OL.py
+++ workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_OL.py
@@ -117,9 +117,10 @@ class QP:
             
             lbz_mod[k*self.nx + 1] = self.y_min
             ubz_mod[k*self.nx + 1] = self.y_max
+        print(f"Zeitschritt {k}: x in [{lbz_mod[k*self.nx + 0]}, {ubz_mod[k*self.nx + 0]}], "
+          f"y in [{lbz_mod[k*self.nx + 1]}, {ubz_mod[k*self.nx + 1]}]")
 
         sol = self.solver(x0 = z0,p=P_val,lbx=lbz_mod, ubx= ubz_mod,lbg = self.lbg, ubg= self.ubg)    
-        self.get_logger().info(f'Received state update: lbz_mod={lbz_mod}, ubz_mod={ubz_mod}')
         
         #sol = self.solver(x0 = z0,p=P_val,lbx=self.lbz, ubx= self.ubz,lbg = self.lbg, ubg= self.ubg)
         z_opt =sol['x'].full().flatten()
