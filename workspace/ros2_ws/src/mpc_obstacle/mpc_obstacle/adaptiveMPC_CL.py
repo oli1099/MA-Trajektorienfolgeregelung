dc5d84351839e5918f6d7ec3792f86a9acd28391
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-03-20 14:54:38 +0100

    EDIT

--- workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_CL.py
+++ workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_CL.py
@@ -73,7 +73,7 @@ class MPCClosedLoop(Node):
         self.x_guess[:,0]=self.x0
         self.u_guess = np.zeros((self.nu,self.N))
 
-        self.slack_guess = np.zeros(self.N)
+        self.slack_guess = np.zeros(self.N-1)
         for i in range(self.N):
             self.x_guess[:,i+1]= self.Ad @ self.x_guess[:,i] + self.Bd @ self.u_guess[:,i]
         for i in range (self.N-1):
@@ -118,7 +118,7 @@ class MPCClosedLoop(Node):
         x_opt, u_opt , slack_opt= self.QP.solveMPC(self.xmeasure, self.x_ref,self.z0)
         u_cl = u_opt[:,0]
         x_cl = x_opt[:,0]
-        self.get_logger().info(f'Received state update: x={x_opt}, y={u_cl}')
+        self.get_logger().info(f'Received state update: x={x_cl}, y={u_cl}')
         self.x_pred =x_opt
         self.predictions_list.append(x_opt.copy())
         
