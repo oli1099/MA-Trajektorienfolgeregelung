fc9fa68ffe0d464aa0be29a5bde8329bbcdec2ae
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-03-21 16:20:52 +0100

    EDIT

--- workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_CL.py
+++ workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_CL.py
@@ -29,7 +29,7 @@ class MPCClosedLoop(Node):
         self.nu = self.mpc_model.nu
 
         # Gewichtsmatrizen festlegen
-        self.Q = np.diag([1,1,0.005,1,1,1]) #Höhere Bestrafung auf der Position
+        self.Q = np.diag([1,1,0.05,1,1,1]) #Höhere Bestrafung auf der Position
         self.R = 0.01*np.eye(self.nu)
         self.QN = self.Q
         self.Penalty = 1e6
--- workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/softMPC_CL.py
+++ workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/softMPC_CL.py
@@ -29,7 +29,7 @@ class MPCClosedLoop(Node):
         self.nu = self.mpc_model.nu
 
         # Gewichtsmatrizen festlegen
-        self.Q = np.diag([1,1,0.001
+        self.Q = np.diag([1,1,0.01
                           ,1,1,1]) #Höhere Bestrafung auf der Position
         self.R = 0.01*np.eye(self.nu)
         self.QN = self.Q
