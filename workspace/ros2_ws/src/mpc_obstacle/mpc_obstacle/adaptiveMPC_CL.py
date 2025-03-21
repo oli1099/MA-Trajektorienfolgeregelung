1b245b640066c074d6793ffc4b7ffafffcf0e4c2
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-03-21 15:01:42 +0100

    EDIT

--- workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_CL.py
+++ workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/adaptiveMPC_CL.py
@@ -34,7 +34,7 @@ class MPCClosedLoop(Node):
         self.Penalty = 1e6
 
         self.Ts = 0.1 #Diskretisierungszeit
-        self.N = 10  #Prediktionshorizont
+        self.N = 15  #Prediktionshorizont
 
         #Mecanum-Chassis Objekt erstellen
         self.mecanum_chassis = MecanumChassis()
