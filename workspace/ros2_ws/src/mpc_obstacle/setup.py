2cdbce75a9d9473a6e97b2fb62f9503ab50ed1ac
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-03-22 13:52:28 +0100

    EDIT

--- workspace/ros2_ws/src/mpc_obstacle/setup.py
+++ workspace/ros2_ws/src/mpc_obstacle/setup.py
@@ -24,6 +24,7 @@ setup(
             'adaptiveMPC_CL = mpc_obstacle.adaptiveMPC_CL:main',
             'slackMPC_CL = mpc_obstacle.slackMPC_CL:main',
             'mpc_CL_Nc = mpc_obstacle.mpc_CL_Nc:main ' ,
-            'mpc_CL_dynObs = mpc_obstacle.mpc_CL_dynObs:main'       ],
+            'mpc_CL_dynObs = mpc_obstacle.mpc_CL_dynObs:main'
+            ],
     },
 )
