1801f2b9864cbe52ff817355ed0e20b6312dd279
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-04-16 17:09:06 +0200

    Plot anpassung Trajectorie

--- workspace/ros2_ws/src/MPC_Trajektorienfolgeregelung/MPC_Trajektorienfolgeregelung/trajectoryPControllerTime.py
+++ workspace/ros2_ws/src/MPC_Trajektorienfolgeregelung/MPC_Trajektorienfolgeregelung/trajectoryPControllerTime.py
@@ -150,7 +150,7 @@ class TrajectoryPController(Node):
 
 
         #Geschwindigkeit Begrenzung
-        v_x = max(min(v_x, 0.2298), -0.2298)
+        v_x = max(min(v_x, 0.2298), -0.22989)
         v_y = max(min(v_y, 0.2298), -0.2298)
 
         theta = max(min(theta,1),-1)
