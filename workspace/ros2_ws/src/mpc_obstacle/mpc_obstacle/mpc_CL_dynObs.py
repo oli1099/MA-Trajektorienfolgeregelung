e95880912d66845bf9c827c0a7dc646932137346
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-04-23 14:11:54 +0200

    In die Spur zurück kehren

--- workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/mpc_CL_dynObs.py
+++ workspace/ros2_ws/src/mpc_obstacle/mpc_obstacle/mpc_CL_dynObs.py
@@ -48,7 +48,6 @@ class MPCClosedLoop(Node):
             'obslength': 0.3 # Breite des Hindernisses
         }
         self.road_width = 2.0  # Breite der Straße (Beispielwert)
-        self.return_distance = 1.5
 
 
         #Mecanum-Chassis Objekt erstellen
@@ -77,7 +76,7 @@ class MPCClosedLoop(Node):
 
         self.xmeasure = None    #Aktuelle gemessene Position des Roboters
         self.xmeasure_received = None 
-        self.x_ref = [4,0,0,0,0,0]
+        self.x_ref = [3,0,0,0,0,0]
         self.x0 = [0,0,0,0,0,0]
         self.u0 = [0.5,0.5,0.5,0.5]
 
@@ -147,7 +146,7 @@ class MPCClosedLoop(Node):
                     cS = np.tan(np.arctan2((obsYrl - carY), (obsXrl - carX)))
                     cI = obsYrl - cS * obsXrl
         else:
-            if carX >= obsXrl + obslength + self.return_distance:# 2*self.Safezone:
+            if carX >= obsXrl + obslength + 2*self.Safezone:
                 cS = 0
                 cI = -self.road_width/2
             else:
