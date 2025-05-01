795aad98ca15a34ba8600abf6711746f9186602a
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-05-01 15:47:29 +0200

    TimeTrajectory neue Trajektorie

--- workspace/ros2_ws/src/MPC_Trajektorienfolgeregelung/MPC_Trajektorienfolgeregelung/Trajectorytracking.py
+++ workspace/ros2_ws/src/MPC_Trajektorienfolgeregelung/MPC_Trajektorienfolgeregelung/Trajectorytracking.py
@@ -68,7 +68,7 @@ class TrajectoryPController(Node):
         self.k_s = 1           # Längsfehler-Gain
 
         self.Ua_max    = 0.2    # maximaler Approach-Speed
-        self.Lp        = 0.15   # fester Look-ahead-Abstand
+        self.Lp        = 0.3   # fester Look-ahead-Abstand
 
         self.start_timer = None
 
