443ac022d0920e6ecc86d3647e9668bdf3e28c54
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-05-18 09:37:43 +0200

    N=50

--- workspace/ros2_ws/src/MPC/MPC/MPC_ClosedLoopTrajectory.py
+++ workspace/ros2_ws/src/MPC/MPC/MPC_ClosedLoopTrajectory.py
@@ -36,7 +36,7 @@ class MPCClosedLoopTrajectory(Node):
         self.QN = self.Q
 
         self.Ts = 0.1 #Disretisierungszeit
-        self.N = 20   #Prediktionshorizont
+        self.N = 50   #Prediktionshorizont
 
         #Mecanum-Chassis Objekt erstellen
         self.mecanum_chassis = MecanumChassis()
