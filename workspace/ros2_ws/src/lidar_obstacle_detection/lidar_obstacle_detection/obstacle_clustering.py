b26dbdd04c99c5af5525d6dca5733b083c83e7b9
oli1099 <oliver.fellenz@stud.tu-darmstadt.de>
2025-05-27 10:05:30 +0200

    EDIT

--- workspace/ros2_ws/src/lidar_obstacle_detection/lidar_obstacle_detection/obstacle_clustering.py
+++ workspace/ros2_ws/src/lidar_obstacle_detection/lidar_obstacle_detection/obstacle_clustering.py
@@ -36,7 +36,7 @@ class LidarClustering(Node):
         points = np.vstack((xs, ys)).T
         
         # Clusterbildung mittels DBSCAN
-        clustering = DBSCAN(eps=0.1, min_samples=7).fit(points)
+        clustering = DBSCAN(eps=0.05, min_samples=10).fit(points)
         labels = clustering.labels_
         
         marker_array = MarkerArray()
